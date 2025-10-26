0b1929fe61943b0e2c7155cba7c808276d710d68
Eduard <eduuardcovrig@gmail.com>
2025-10-26 20:08:11 +0200

    Adaugat Seminar7 + o mica eroare la seminar 5

--- Seminar5/script1_main.js
+++ Seminar5/script1_main.js
@@ -1,6 +1,6 @@
-import {a as Value, Sum, Product } from "./math.js";
+import {a as Value, Sum, Product } from "./script1_math.js";
 // import * as Math from "./math.js";
 //console.log(Math.a); // console.log(Math.Sum(1,2)); etc
 console.log(Value);
 console.log(Sum(1,2));
-console.log(Product(2,3));A
+console.log(Product(2,3));
